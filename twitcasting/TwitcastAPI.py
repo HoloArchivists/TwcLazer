314f9ea347cfffc6cd1123b1b98ab28f11cabb5f
user <user@localhost>
2023-07-03 03:27:03 +0000

    Add cookies support

--- twitcasting/TwitcastAPI.py
+++ twitcasting/TwitcastAPI.py
@@ -1,27 +1,27 @@
 # Twitcasting Downloader API Functions
 import requests
 import json
+
 import twitcasting.TwitcastStream as TwitcastStream
+from utils.CookiesHandler import CookiesHandler
 
 class TwitcastingAPI:
     '''Class for the functional Twitcasting API'''
     
     # Get Token
-    @staticmethod
-    def GetToken(movieID) -> TwitcastStream.HappyToken:
+    def GetToken(self, movieID) -> TwitcastStream.HappyToken:
         HappyTokenURL = "https://twitcasting.tv/happytoken.php"
         HappyTokenData = {'movie_id': movieID}
-        HappyTokenRequest = requests.post(HappyTokenURL, data=HappyTokenData)
+        HappyTokenRequest = self.session.post(HappyTokenURL, data=HappyTokenData)
         
         HappyTokenRequestData = json.loads(HappyTokenRequest.text)
         
         return TwitcastStream.HappyToken(HappyTokenRequestData["token"])
         
     # Get Current Stream
-    @staticmethod
-    def GetStream(username) -> TwitcastStream.StreamServer:
+    def GetStream(self, username) -> TwitcastStream.StreamServer:
         StreamServerURL = f"https://twitcasting.tv/streamserver.php?target={username}&mode=client"
-        StreamServer_Request = requests.get(StreamServerURL)
+        StreamServer_Request = self.session.get(StreamServerURL)
         
         if StreamServer_Request.status_code != 200:
             print("User is not live or invalid username.")
@@ -45,10 +45,9 @@ class TwitcastingAPI:
         )
     
     # Get Stream Info
-    @staticmethod
-    def GetStreamInfo(movieID, token) -> TwitcastStream.TwitcastStream_:
+    def GetStreamInfo(self, movieID, token) -> TwitcastStream.TwitcastStream_:
         TwitcastStreamURL = f"https://frontendapi.twitcasting.tv/movies/{movieID}/status/viewer?token={token.token}"
-        TwitcastStream_Request = requests.get(TwitcastStreamURL)
+        TwitcastStream_Request = self.session.get(TwitcastStreamURL)
         
         if TwitcastStream_Request.status_code != 200:
             print("User is not live or invalid username.")
@@ -69,28 +68,40 @@ class TwitcastingAPI:
             TwitcastStreamData.get("movie").get("pin_message"))
         
     # Get PubSub URL
-    @staticmethod
-    def GetPubSubURL(movieID) -> TwitcastStream.EventsPubSubURL:
+    def GetPubSubURL(self, movieID) -> TwitcastStream.EventsPubSubURL:
         PubSubURL = "https://twitcasting.tv/eventpubsuburl.php"
         PubSubData = {'movie_id': movieID}
-        PubSubRequest = requests.post(PubSubURL, data=PubSubData)
+        PubSubRequest = self.session.post(PubSubURL, data=PubSubData)
         
         PubSubRequestData = json.loads(PubSubRequest.text)
         
         return TwitcastStream.EventsPubSubURL(PubSubRequestData["url"])
     
     @staticmethod
-    def is_live(username) -> bool:
-        is_user_live = requests.get(f"https://twitcasting.tv/userajax.php?c=islive&u={username}")
+    def user_is_live(username, cookies=None) -> bool:
+        url = f"https://twitcasting.tv/userajax.php?c=islive&u={username}"
+        is_user_live = requests.get(url, cookies=cookies)
+        if is_user_live.status_code != 200:
+            print(f"got status {is_user_live.status_code} when checking if user {username} is live, '{is_user_live.text}'")
         if is_user_live.text == "0":
             return False
         else:
             return True
-    
+
+    def is_live(self):
+        return self.user_is_live(self.userInput["username"], self.session.cookies)
+
+    @property
+    def cookies_header(self):
+        return CookiesHandler.get_cookies_header(self.session.cookies, "https://twitcasting.tv")
+
     def __init__(self, UserInput) -> None:
         # Input TwitcastStream.py Userinput object
         self.userInput = UserInput
-        self.CurrentStream = TwitcastingAPI.GetStream(self.userInput["username"])
-        self.CurrentStreamToken = TwitcastingAPI.GetToken(self.CurrentStream.movie_id)
-        self.CurrentStreamInfo = TwitcastingAPI.GetStreamInfo(self.CurrentStream.movie_id, self.CurrentStreamToken)
-        self.CurrentStreamPubSubURL = TwitcastingAPI.GetPubSubURL(self.CurrentStream.movie_id)
+        self.session = requests.Session()
+        self.session.cookies = self.userInput["cookies"] or requests.cookies.RequestsCookieJar()
+
+        self.CurrentStream = self.GetStream(self.userInput["username"])
+        self.CurrentStreamToken = self.GetToken(self.CurrentStream.movie_id)
+        self.CurrentStreamInfo = self.GetStreamInfo(self.CurrentStream.movie_id, self.CurrentStreamToken)
+        self.CurrentStreamPubSubURL = self.GetPubSubURL(self.CurrentStream.movie_id)
